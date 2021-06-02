199022a64aa55d0117792798da4d9a039511a515
Quinn Armentrout <quinnarmentrout@gmail.com>
2021-06-01 20:32:47 -0700

    made combinedshape a bit cleaner

--- src/CombinedShape.java
+++ src/CombinedShape.java
@@ -1,17 +1,21 @@
 import java.util.HashSet;
 
 public class CombinedShape extends Shape{
-
     public CombinedShape(Shape[] components) {
         this.pos = components[0].pos;
-        this.points = new HashSet<int[]>();
+        this.points = combineShapes(components);
+    }
+
+    public static HashSet<int[]> combineShapes(Shape[] components) {
+        HashSet<int[]> outPoints = new HashSet<int[]>();
+        int[] position = components[0].pos;
         for (Shape shape : components) {
+            int relativeX = shape.pos[0]-position[0];
+            int relativeY = shape.pos[1]-position[1];
             for (int[] point : shape.points) {
-                int relativeX = (shape.pos[0]-this.pos[0])+point[0];
-                int relativeY = (shape.pos[1]-this.pos[1])+point[1];
-                this.points.add(new int[] {relativeX, relativeY});
+                outPoints.add(new int[] {relativeX+point[0], relativeY+point[1]});
             }
         }
+        return outPoints;
     }
-    
 }
