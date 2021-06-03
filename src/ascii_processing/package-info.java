7a79cef002ade7bc49fe469b52c22924793f0f28
Quinn Armentrout <quinnarmentrout@gmail.com>
2021-06-02 22:09:14 -0700

    added package-info

---
+++ src/ascii_processing/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the classes for 
+ * a barebones Processing clone
+ * using ASCII characters.
+ * 
+ * <p>To use, edit the {@link EditCode} class. 
+ * 
+ * @see <a href="https://processing.org/">Processing</a>
+ */
+package ascii_processing;
