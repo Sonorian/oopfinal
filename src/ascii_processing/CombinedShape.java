1e6b2c5d0f7559ff2922aedd942a980c25e92956
Quinn Armentrout <quinnarmentrout@gmail.com>
2021-06-03 12:50:34 -0700

    added intersect- and reverseIntersectShape methods

--- src/ascii_processing/CombinedShape.java
+++ src/ascii_processing/CombinedShape.java
@@ -42,10 +42,46 @@ public class CombinedShape extends Shape{
      */
     public final void removeShape(Shape shape) {
         int relativeX = shape.pos[0] - this.pos[0];
-            int relativeY = shape.pos[1] - this.pos[1];
+        int relativeY = shape.pos[1] - this.pos[1];
             for (int[] point : shape.points) {
                 this.points.remove(new int[]
                     {relativeX + point[0], relativeY + point[1]});
+            }
+    }
+
+    /**
+     * Removes the points of this shape that aren't part of another shape. 
+     * 
+     * @param shape {@link Shape} to intersect this shape with
+     */
+    public final void intersectShape(Shape shape) {
+        int relativeX = this.pos[0] - shape.pos[0]; // relative to shape's position,
+        int relativeY = this.pos[1] - shape.pos[1]; // unlike all other methods
+            for (int[] point : this.points) {
+                int[] relativePoint = new int[]
+                    {relativeX + point[0], relativeY + point[1]};
+                if (shape.points.contains(relativePoint) == false) {
+                    this.points.remove(point);
+                }
+            }
+    }
+
+    /**
+     * Combines this shape with another, excluding points shared by both. 
+     * 
+     * @param shape {@link Shape} to combine this shape with
+     */
+    public final void reverseIntersectShape(Shape shape) {
+        int relativeX = shape.pos[0] - this.pos[0];
+        int relativeY = shape.pos[1] - this.pos[1];
+            for (int[] point : shape.points) {
+                int[] relativePoint = new int[]
+                    {relativeX + point[0], relativeY + point[1]};
+                if (this.points.contains(relativePoint) == false) {
+                    this.points.add(point);
+                } else {
+                    this.points.add(point);
+                }
             }
     }
 }
