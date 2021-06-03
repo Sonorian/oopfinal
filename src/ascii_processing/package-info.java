0854e0643837e5d8be57dd89d63b053536595f11
Quinn Armentrout <quinnarmentrout@gmail.com>
2021-06-02 22:23:06 -0700

    i don't remember changing things, but vscode says i did

--- src/ascii_processing/package-info.java
+++ src/ascii_processing/package-info.java
@@ -1,9 +1,10 @@
 /**
- * Provides the classes for 
- * a barebones Processing clone
- * using ASCII characters.
+ * Provides the classes for a barebones
+ * Processing clone using ASCII characters.
  * 
  * <p>To use, edit the {@link EditCode} class. 
+ * 
+ * <p><a href="https://github.com/Sonorian/oopfinal">Source</a>
  * 
  * @see <a href="https://processing.org/">Processing</a>
  */
